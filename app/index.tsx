7f412f5a6b0ee3f239008b04788aa8b96b133c48
Uspex918 <uspex964@gmail.com>
2026-08-27 15:40:05 +0400

    native app

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Link } from "expo-router"
+import { View } from "react-native"
+
+import { styles } from "../styles/auth.styles"
+
+export default function Index() {
+    return (
+        <View style={styles.container}>
+            <Link href={"/notifications"}>visit notification screen</Link>
+            <Link href={"/profile"}>visit profile screen</Link>
+        </View>
+    )
+}
